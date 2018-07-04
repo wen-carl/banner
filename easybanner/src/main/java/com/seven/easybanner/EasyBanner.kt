7a484631202aa963014a4c10abac4790cbc0593e
Carl <wen_carl0701@163com>
2018-07-04 23:26:55 +0800

    Try something.

--- easybanner/src/main/java/com/seven/easybanner/EasyBanner.kt
+++ easybanner/src/main/java/com/seven/easybanner/EasyBanner.kt
@@ -3,47 +3,66 @@ package com.seven.easybanner
 import android.content.Context
 import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
+import android.util.AttributeSet
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.seven.easybanner.adapter.BaseAdapter
 import android.widget.FrameLayout
 
-class EasyBanner(context: Context) : FrameLayout(context), ViewPager.OnPageChangeListener {
+class EasyBanner(private val build: Build, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : FrameLayout(build.context, attrs, defStyleAttr) {
 
-    private var baseAdapter: BaseAdapter? = null
+    private lateinit var mViewPager: ViewPager
 
-    /**
-     * android.support.v4.view.ViewPager.OnPageChangeListener}
-     */
-    override fun onPageScrollStateChanged(state: Int) {
+    init {
+        val mainContent = LayoutInflater.from(build.context).inflate(R.layout.layout_easy_banner, this, true)
+        mViewPager = mainContent.findViewById(R.id.banner_content)
+        mViewPager.adapter = build.mAdapter
+    }
+
+    fun start() {
 
     }
 
-    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+    fun stop() {
 
     }
 
-    override fun onPageSelected(position: Int) {
+    fun pause() {
 
     }
 
-    class Adapter(val adapter: BaseAdapter) : PagerAdapter() {
+    fun showPrevious() {
 
-        override fun instantiateItem(container: ViewGroup, position: Int): Any {
-            return adapter.onCreatView(adapter.context, position) as Any
+    }
+
+    fun showNext() {
+
+    }
+
+    fun show(index: Int) {
+
+    }
+
+    class Build(val context: Context) {
+
+        var isAutoPlay = true
+        var timeInterval = 2.0f
+        var mAdapter: BaseAdapter? = null
+
+        fun setAutoPlay(enable: Boolean) : Build {
+            isAutoPlay = enable
+            return this
         }
 
-        override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
-            //super.destroyItem(container, position, `object`)
-            container.removeView(`object` as View)
+        fun setTimeInterval(interval: Float) : Build {
+            timeInterval = interval
+            return this
         }
 
-        override fun isViewFromObject(view: View, `object`: Any): Boolean {
-            return view == `object`
-        }
-
-        override fun getCount(): Int {
-            return adapter.data.size
+        fun setAdapter(adapter: BaseAdapter) : Build {
+            mAdapter = adapter
+            return this
         }
     }
 }
--- easybanner/src/main/java/com/seven/easybanner/adapter/BaseAdapter.kt
+++ easybanner/src/main/java/com/seven/easybanner/adapter/BaseAdapter.kt
@@ -4,20 +4,44 @@ import android.content.Context
 import android.support.v4.view.PagerAdapter
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.seven.easybanner.model.Data
+
+private const val DEFAULT_TYPE = -1
+private const val POSITION_LEFT = 0
+private const val POSITION_CENTER = 1
+private const val POSITION_RIGHT = 2
+private const val POSITION_UNUSED = 3
 
 abstract class BaseAdapter(val context: Context, var data: List<Data>) : PagerAdapter() {
 
-    private val mViews: MutableMap<Int, MutableList<View>> = mutableMapOf()
+    private val mViews: MutableMap<Int, MutableMap<Int, View>> = mutableMapOf()
+    private val mPosition = mutableListOf<Int>()
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
 
-        if (mViews.size < 3) {
-            val view = onCreatView(context, position)
+        val type = getViewType(position)
+        var tempMap = mViews[type]
 
+        if (null == tempMap) {
+            tempMap = mutableMapOf()
+            mViews[type] = tempMap
         }
 
-        return onCreatView(context, position) as Any
+        val view: View
+        if (tempMap.size < 3) {
+            view = onCreatView(context, position, type)
+
+        } else {
+            view = tempMap[POSITION_UNUSED]!!
+        }
+
+        val left = tempMap[POSITION_LEFT]
+        val center = tempMap[POSITION_CENTER]
+        val right = tempMap[POSITION_RIGHT]
+        onDisplay(context, view, position, data[position])
+
+        return view
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
@@ -34,10 +58,10 @@ abstract class BaseAdapter(val context: Context, var data: List<Data>) : PagerAd
     }
 
     open fun getViewType(position: Int) : Int {
-        return -1
+        return DEFAULT_TYPE
     }
 
-    abstract fun onCreatView(context: Context, position: Int) : View?
+    abstract fun onCreatView(context: Context, position: Int, viewType: Int) : View
 
     abstract fun onDisplay(context: Context, view: View, position: Int, model: Data)
 }
